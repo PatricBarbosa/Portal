4e2d835c3a3f1f0f40a051d4fa7b496b0dc6a2d8
Patric Barbosa <98431700+PatricBarbosa@users.noreply.github.com>
2022-11-23 11:22:56 -0300

    att

--- JS/assessoria.js
+++ JS/assessoria.js
@@ -1,9 +1,36 @@
-const handlesubmit =  (Event) => {
-    Event.preventDefault();
+//const chamada =  (Event) => {
+    //Event.preventDefault();
 
     //const assessoria = document.querySelector('input[name=data[assessoria]]').value;
 
-    fetch('https://sheetdb.io/api/v1/rzyubi4ewa8ri');
-}
+    //fetch('https://sheetdb.io/api/v1/rzyubi4ewa8ri');
+//}
 
-document.querySelector('form').addEventListener('submit', handlesubmit);
+//document.querySelector('form').addEventListener('submit', chamada);
+
+
+Event.preventDefault();
+
+fetch('https://sheetdb.io/api/v1/rzyubi4ewa8ri', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+}).then((result) => {
+  // Handle the result
+});
+
+const chamada = (event) =>{
+    event.preventDefault();
+
+    fetch('https://sheetdb.io/api/v1/rzyubi4ewa8ri', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+}).then((result) => {
+  // Handle the result
+});
+} 
